503ac5a739fdbada524963ffdd7c26400c947f06
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-08-15 16:05:27 +0000

    second upload

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   devise_for :users
   get '/about' => "homes#about"
 
-  resources :postimages, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+
   resources :users, only: [:show, :edit, :update]
 
 end
