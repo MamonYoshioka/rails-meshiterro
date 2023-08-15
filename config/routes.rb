e63b178d81196832b721a77c59b6410cfdfc72c5
MamonYoshioka <141148990+MamonYoshioka@users.noreply.github.com>
2023-08-15 03:52:36 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  root to: "homes#top"
+
+  devise_for :users
+  get '/about' => "homes#about"
+
+  resources :postimages, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
+
+end
